efbe3c92c1d50b6c0fcb4a037cb80ff1dcac6a22
Guilherme Souza <grsouza@pm.me>
2022-06-23 08:25:04 -0300

    Implement CodeTextField component

--- Sources/CodeTextField/CodeTextField.swift
+++ Sources/CodeTextField/CodeTextField.swift
@@ -1,6 +1,110 @@
-public struct CodeTextField {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct CodeTextField: View {
+  @Binding var code: String
+  let numberOfDigits: Int
+  let spacing: CGFloat?
+  let autofocus: Bool
+
+  @Environment(\.codeTextFieldStyle) var codeTextFieldStyle
+  @FocusState private var isTextFieldFocused: Bool
+
+  var focusedIndex: Int? {
+    guard isTextFieldFocused else {
+      return nil
     }
+
+    if code.count == numberOfDigits {
+      return numberOfDigits - 1
+    }
+
+    return code.count
+  }
+
+  public init(
+    code: Binding<String>,
+    numberOfDigits: Int,
+    spacing: CGFloat? = nil,
+    autofocus: Bool = true
+  ) {
+    _code = Binding(
+      get: { String(code.wrappedValue.filter(\.isNumber).prefix(numberOfDigits)) },
+      set: { newValue, transaction in
+        code.transaction(transaction).wrappedValue = String(
+          newValue.filter(\.isNumber).prefix(numberOfDigits))
+      }
+    )
+    self.numberOfDigits = numberOfDigits
+    self.spacing = spacing
+    self.autofocus = autofocus
+  }
+
+  public var body: some View {
+    ZStack {
+      HStack(spacing: spacing) {
+        ForEach(0..<numberOfDigits, id: \.self) { index in
+          codeTextFieldStyle.makeBody(
+            configuration: CodeTextFieldStyleConfiguration(
+              label: Text(digit(at: index)),
+              isFocused: focusedIndex == index
+            )
+          )
+        }
+      }
+
+      TextField("", text: $code)
+        .hidden()
+        .textContentType(.oneTimeCode)
+      #if os(iOS)
+        .keyboardType(.numberPad)
+      #endif
+      .focused($isTextFieldFocused)
+      .foregroundColor(.clear)
+      .accentColor(.clear)
+      .onChange(of: code) { newValue in
+        if newValue.count == numberOfDigits {
+          isTextFieldFocused = false
+        }
+      }
+      .task {
+        if autofocus {
+          try? await Task.sleep(nanoseconds: NSEC_PER_MSEC * 500)
+          isTextFieldFocused = true
+        }
+      }
+    }
+  }
+
+  private func digit(at index: Int) -> String {
+    let characters = Array(code)
+    if index < characters.count {
+      return String(characters[index])
+    }
+
+    return ""
+  }
 }
+
+#if DEBUG
+  struct CodeTextField_Previews: PreviewProvider {
+
+    struct Preview: View {
+      @State var code = "123456"
+
+      var body: some View {
+        CodeTextField(code: $code, numberOfDigits: 6)
+          .padding()
+      }
+    }
+
+    static var previews: some View {
+      Group {
+        Preview()
+          .preferredColorScheme(.light)
+        Preview()
+          .preferredColorScheme(.dark)
+      }
+
+    }
+  }
+#endif
---
+++ Sources/CodeTextField/CodeTextFieldStyle.swift
@@ -0,0 +1,73 @@
+import SwiftUI
+
+/// The properties of a code text field.
+public struct CodeTextFieldStyleConfiguration {
+    public typealias Label = Text
+
+  /// A digit of a code text field.
+  public let label: Label
+
+  /// A boolean that indicates wheter this digit is currently focused.
+  public let isFocused: Bool
+}
+
+/// A type that applies standard interaction behavior and a custom appearance to all code text fields within a view hierarchy.
+public protocol CodeTextFieldStyle {
+  typealias Configuration = CodeTextFieldStyleConfiguration
+  associatedtype Body: View
+
+  /// Creates a view that represents the body of a button.
+  func makeBody(configuration: Configuration) -> Body
+}
+
+public struct DefaultCodeTextFieldStyle: CodeTextFieldStyle {
+  public init() {}
+
+  public func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .multilineTextAlignment(.center)
+      .font(.title)
+      .padding()
+      .background(.regularMaterial)
+      .cornerRadius(12)
+      .scaleEffect(configuration.isFocused ? 1.2 : 1.0)
+  }
+}
+
+extension CodeTextFieldStyle where Self == DefaultCodeTextFieldStyle {
+  public static var `default`: DefaultCodeTextFieldStyle { DefaultCodeTextFieldStyle() }
+}
+
+public struct AnyCodeTextFieldStyle: CodeTextFieldStyle {
+
+  private var bodyBuild: (Configuration) -> AnyView
+
+  init<S: CodeTextFieldStyle>(_ style: S) {
+    self.bodyBuild = { configuration in
+      AnyView(style.makeBody(configuration: configuration))
+    }
+  }
+
+  public func makeBody(configuration: Configuration) -> AnyView {
+    bodyBuild(configuration)
+  }
+}
+
+private enum CodeTextFieldStyleEnvironmentKey: EnvironmentKey {
+  static var defaultValue: AnyCodeTextFieldStyle {
+    AnyCodeTextFieldStyle(.default)
+  }
+}
+
+extension EnvironmentValues {
+  public var codeTextFieldStyle: AnyCodeTextFieldStyle {
+    get { self[CodeTextFieldStyleEnvironmentKey.self] }
+    set { self[CodeTextFieldStyleEnvironmentKey.self] = newValue }
+  }
+}
+
+extension View {
+  public func codeTextFieldStyle<S: CodeTextFieldStyle>(_ style: S) -> some View {
+    environment(\.codeTextFieldStyle, AnyCodeTextFieldStyle(style))
+  }
+}
